80de5d5e849a8c458bdcd74e9a207a3fb1b34d83
adii07 <adityasharma5119@gmail.com>
2022-02-06 07:48:17 +0530

    registeration

--- src/components/register.js
+++ src/components/register.js
@@ -8,19 +8,57 @@ const LoginRegister = () => {
   const [userName,setName]= useState('');
   const [isRestraunt,setRest]=useState(false);
   let navigate=useNavigate();
+  const url='http://localhost:8000/restraunt';
 
   const redirect=(e)=>{
-    if(userName==='' || userEmail==='' || userCity==='')return;
-
+    if(userName==='' || userEmail==='' || userCity===''){ e.preventDefault();}
     localStorage.setItem("name",userName.toUpperCase());
     localStorage.setItem("email",userEmail.toUpperCase());
     localStorage.setItem("city",userCity.toUpperCase());
     localStorage.setItem("isRestraunt",isRestraunt); 
-    console.log(userName,userEmail,userCity);
+    // console.log(userName,userEmail,userCity);
   }
   const navigateToDash=()=>{
     redirect();
+    register();
     navigate('/restraunt');
+  }
+
+  function register(){
+    const abortControl=new AbortController();
+    setTimeout(()=>{
+        fetch(url,{signal:abortControl.signal})
+        .then(res=>{
+            if(!res.ok) throw Error('Could not fetch the data');
+            return res.json();
+        })
+        .then(data=>{
+            const req=data.findIndex(({name})=>name===userName);
+            if(req!==-1) console.log("Alread present");
+            else{
+              const cred={
+                "name": userName.toLocaleUpperCase(),
+                "city": userCity.toUpperCase()
+              }
+              // console.log(cred);
+              fetch('http://localhost:8000/restraunt', {
+              method: 'POST',
+              body: JSON.stringify(cred),
+              headers: { 'Content-Type': 'application/json' }
+            })
+            }
+        })
+        .catch((err)=>{
+            if(err.name==="AbortError"){
+                console.log("Aborted");
+            }
+        })
+},1000);
+  }
+
+  const navigateToHome=()=>{
+    redirect();
+    navigate('/user');
   }
   return (
     <div className={`container ${addclass}`} id="container">
@@ -31,7 +69,7 @@ const LoginRegister = () => {
           <input type="email" required placeholder="EMAIL" onChange={e=>{setEmail(e.target.value)}} />
           <input type="text" required placeholder="CITY" onChange={e=>{setCity(e.target.value)}} />
           <input type="password" required placeholder="PASSWORD" onChange={e=>{setRest(true)}} />
-         <button type="submit"  onClick={navigateToDash}>REGISTER</button>
+         <button type="button"  onClick={navigateToDash}>REGISTER</button>
          {/* <Link to='/restraunt'></Link>*/}
         </form>
       </div>
@@ -42,7 +80,7 @@ const LoginRegister = () => {
           <input type="email" required placeholder="EMAIL"  onChange={(e)=>setEmail(e.target.value)} />
           <input type="text" required placeholder="CITY" onChange={e=>{setCity(e.target.value)}} />
           <input type="password" required placeholder="PASSWORD" onChange={e=>{setRest(false)}}/>
-          <Link to='/user'><button type="submit"  onClick={redirect}>Sign in</button></Link>
+          <Link to='/user'><button type="submit"  onClick={navigateToHome}>Sign in</button></Link>
         </form>
       </div>
       <div className="overlay-container">
--- src/components/restraunts.js
+++ src/components/restraunts.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import'./menu.css'
 const RES=({menu})=>{
     const[res,setRes]=useState(null);
